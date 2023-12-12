5b49d4bd35217d8fbef726c1206af4254950c716
Lance Crisang <lancecrisang14@gmail.com>
2023-12-12 09:40:02 +0800

    add: day 12 blank project

---
+++ Day12/Program.cs
@@ -0,0 +1,27 @@
+﻿using Xapier14.AdventOfCode;
+AdventOfCode.SetYearAndDay(2023, 12);
+
+var input = AdventOfCode.GetInputLines();
+
+var sample =
+    """
+    
+    
+    """.Split(Environment.NewLine);
+
+Utility.Assert(Part1, input, 0);
+Utility.Assert(Part2, input, 0);
+
+Console.WriteLine("Part 1: {0}", Part1(input));
+Console.WriteLine("Part 2: {0}", Part2(input));
+return;
+
+long Part1(string[] lines)
+{
+    return 0;
+}
+
+long Part2(string[] lines)
+{
+    return 0;
+}
